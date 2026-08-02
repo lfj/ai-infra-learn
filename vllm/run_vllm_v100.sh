a14d5fa693146c12fe095c777b6d67941dc567cf
lifujun006 <lifujun006@ke.com>
2026-08-02 13:00:58 +0800

    提交shell

--- vllm/run_vllm_v100.sh
+++ vllm/run_vllm_v100.sh
@@ -9,7 +9,6 @@ OMP_NUM_THREADS=4
 
 python -m vllm.entrypoints.openai.api_server \
 --model /root/autodl-tmp/models/models/qwen--Qwen2.5-7B-Instruct/snapshots/master \
---load-format local \
 --served-model-name qwen-7b \
 --host 0.0.0.0 \
 --port 6006 \
